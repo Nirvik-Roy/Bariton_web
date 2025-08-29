630a2eb228bb4353a192b0402632b6e887f99e56
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-08-29 12:00:42 +0530

    'up'

--- components/PrivacyPolicy/PrivacyPolicyContent.js
+++ components/PrivacyPolicy/PrivacyPolicyContent.js
@@ -5,49 +5,80 @@ const PrivacyPolicyContent = () => {
     <>
       <div className="text-container ptb-110">
         <div className="container">
-        <h4>Effective Date: 29-08-2025</h4>
-        <p>Grue and Bleen Technologies Pvt. Ltd. (“we,” “our,” or “us”) is committed to protecting your privacy. This Privacy Policy outlines how we collect, use, disclose, and safeguard your information when you visit our website or engage with our services.</p>
+          <h4>Effective Date: 29-08-2025</h4>
+          <p>
+            Grue and Bleen Technologies Pvt. Ltd. (“we,” “our,” or “us”) is
+            committed to protecting your privacy. This Privacy Policy outlines
+            how we collect, use, disclose, and safeguard your information when
+            you visit our website or engage with our services.
+          </p>
           <h4>1. Information We Collect</h4>
 
           <p>
-             <b>a. Personal Information:</b> -Name -Email address -Contact number -Company name - Job title (if provided)
+            <b>a. Personal Information:</b> -Name -Email address -Contact number
+            -Company name - Job title (if provided)
           </p>
           <p>
-             <b>b. Technical Information:</b> -IP address -Browser type and version -Device type - pages visited and time spent on the website - Referring website URLs
+            <b>b. Technical Information:</b> -IP address -Browser type and
+            version -Device type - pages visited and time spent on the website -
+            Referring website URLs
           </p>
           <p>
-             <b>c. Cookies and Tracking:</b> we use cookies and similar tracking technologies to improve your browsing experience and analyze traffic. You can control cookie preference in your browser settings.
+            <b>c. Cookies and Tracking:</b> we use cookies and similar tracking
+            technologies to improve your browsing experience and analyze
+            traffic. You can control cookie preference in your browser settings.
           </p>
 
           <h4>2. How We Use Your Information</h4>
           <p>
-            We may use your information to:- Respond to inquires and provide support - Send updates or service-related communications - Improve our website functionality and security - Personalize content and User experience - Comply with legal or regulatory obligations.
+            We may use your information to:- Respond to inquires and provide
+            support - Send updates or service-related communications - Improve
+            our website functionality and security - Personalize content and
+            User experience - Comply with legal or regulatory obligations.
           </p>
-
 
           <h4>3. Information Sharing and Disclosure</h4>
           <p>
-            We do not sell or rent your personal data. We may share your information:- With Service providers for hosting, analytics, or marketing support - If required by law or government authorities - In the event of a merger, acquisition, or company restructuring
+            We do not sell or rent your personal data. We may share your
+            information:- With Service providers for hosting, analytics, or
+            marketing support - If required by law or government authorities -
+            In the event of a merger, acquisition, or company restructuring
           </p>
 
-        <h4>4. Data Security</h4>
+          <h4>4. Data Security</h4>
           <p>
-            We implement industry-standard measures to protect your data from unauthorized access, alteration, disclosure, or destruction. However, no method of transmission over the internet id 100% secure.
+            We implement industry-standard measures to protect your data from
+            unauthorized access, alteration, disclosure, or destruction.
+            However, no method of transmission over the internet id 100% secure.
           </p>
 
           <h4>5. Your Rights and Choices</h4>
           <p>
-            You may:- Request access to or correction of your data - Withdraw consent at any time - Opt out of marketing communications by contacting us
+            You may:- Request access to or correction of your data - Withdraw
+            consent at any time - Opt out of marketing communications by
+            contacting us
           </p>
-          <p>To exercise your rights, email us at: <b>privacy@gruebleen.com</b></p>
+          <p>
+            To exercise your rights, email us at: <b>privacy@gruebleen.com</b>
+          </p>
           <h4>6. Third-Party Links</h4>
-          <p>Our website may contain links to other websites. We are not responsible for their privacy practices. Please review their privacy policies individually.</p>
+          <p>
+            Our website may contain links to other websites. We are not
+            responsible for their privacy practices. Please review their privacy
+            policies individually.
+          </p>
 
           <h4>7. Changes to This Policy</h4>
-          <p>We may update this Privacy Policy periodically. Any changes will be posted here with a revised effective data.</p>
+          <p>
+            We may update this Privacy Policy periodically. Any changes will be
+            posted here with a revised effective data.
+          </p>
 
           <h4>8. Contact Us</h4>
-          <p>For questions about this Privacy Policy or your data: 📩 <b>privacy@greubleen.com</b> </p>
+          <p>
+            For questions about this Privacy Policy or your data: 📩{" "}
+            <b>privacy@greubleen.com</b>{" "}
+          </p>
           <p>📞 [Insert phone number]</p>
           <p>📍 [Insert address line, if desired]</p>
         </div>
